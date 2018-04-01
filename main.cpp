c28549f027901c5d59a49642ac53d06ab66c8df2
saranshiitd <cs1160326@cse.iitd.ac.in>
2018-04-01 18:57:27 +0530

    merge

--- main.cpp
+++ main.cpp
@@ -312,10 +312,30 @@ int main(int argc, char *argv[])
     QLabel l;
     QPicture pi;
     QPainter p(&pi);
+    QLabel l2 ;
+    QPicture pi2 ;
+    QPainter p2(&pi2) ;
+    QLabel l3 ;
+    QPicture pi3 ;
+    QPainter p3(&pi3) ;
+    QLabel l4 ;
+    QPicture pi4 ;
+    QPainter p4(&pi4) ;
     edge2D currentEdge ;
     p.setRenderHint(QPainter::Antialiasing);
     p.setPen(QPen(Qt::black, 1, Qt::SolidLine, Qt::RoundCap));
     p.scale(0.9,-0.9);
+    p2.setRenderHint(QPainter::Antialiasing);
+    p2.setPen(QPen(Qt::black, 1, Qt::SolidLine, Qt::RoundCap));
+    p2.scale(0.9,-0.9);
+    p3.setRenderHint(QPainter::Antialiasing);
+    p3.setPen(QPen(Qt::black, 1, Qt::SolidLine, Qt::RoundCap));
+    p3.scale(0.9,-0.9);
+    p4.setRenderHint(QPainter::Antialiasing);
+    p4.setPen(QPen(Qt::black, 1, Qt::SolidLine, Qt::RoundCap));
+    p4.scale(0.9,-0.9);
+
+
 //    p.translate(-600,-600);
 
     // p.drawLine(0, 0, 200, 200);
@@ -330,20 +350,24 @@ int main(int argc, char *argv[])
         currentEdge = sideViewHidden[i] ;
         firstPoint = currentEdge.v1.getCoordinates() ;
         secondPoint = currentEdge.v2.getCoordinates() ;
-        p.drawLine(firstPoint[0],firstPoint[1],secondPoint[0],secondPoint[1] );
+        p2.drawLine(firstPoint[0],firstPoint[1],secondPoint[0],secondPoint[1] );
     }
     for(unsigned i = 0 ; i < topViewHidden.size();i+=1){
         currentEdge = topViewHidden[i] ;
         firstPoint = currentEdge.v1.getCoordinates() ;
         secondPoint = currentEdge.v2.getCoordinates() ;
-        p.drawLine(firstPoint[0],firstPoint[1],secondPoint[0],secondPoint[1] );
+        p3.drawLine(firstPoint[0],firstPoint[1],secondPoint[0],secondPoint[1] );
     }
-    p.setPen(QPen(Qt::black, 3, Qt::SolidLine, Qt::RoundCap));
+    p.setPen(QPen(Qt::black, 3, Qt::SolidLine, Qt::RoundCap));    
+    p2.setPen(QPen(Qt::black, 3, Qt::SolidLine, Qt::RoundCap));
+    p3.setPen(QPen(Qt::black, 3, Qt::SolidLine, Qt::RoundCap));
+    p4.setPen(QPen(Qt::black, 3, Qt::SolidLine, Qt::RoundCap));
+
     for(unsigned i = 0 ; i < topViewEdges.size();i+=1){
         currentEdge = topViewEdges[i] ;
         firstPoint = currentEdge.v1.getCoordinates() ;
         secondPoint = currentEdge.v2.getCoordinates() ;
-        p.drawLine(firstPoint[0],firstPoint[1],secondPoint[0],secondPoint[1] );
+        p3.drawLine(firstPoint[0],firstPoint[1],secondPoint[0],secondPoint[1] );
     }
     for(unsigned i = 0 ; i < frontViewEdges.size();i+=1){
         currentEdge = frontViewEdges[i] ;
@@ -355,17 +379,17 @@ int main(int argc, char *argv[])
         currentEdge = sideViewEdges[i] ;
         firstPoint = currentEdge.v1.getCoordinates() ;
         secondPoint = currentEdge.v2.getCoordinates() ;
-        p.drawLine(firstPoint[0],firstPoint[1],secondPoint[0],secondPoint[1] );
+        p2.drawLine(firstPoint[0],firstPoint[1],secondPoint[0],secondPoint[1] );
     }
     for(unsigned i = 0 ; i < isometricView.size();i+=1){
         currentEdge = isometricView[i] ;
         firstPoint = currentEdge.v1.getCoordinates() ;
         secondPoint = currentEdge.v2.getCoordinates() ;
-        p.drawLine(firstPoint[0],firstPoint[1],secondPoint[0],secondPoint[1] );
+        p4.drawLine(firstPoint[0],firstPoint[1],secondPoint[0],secondPoint[1] );
     }
-    float* set = twodObj->getMinXY() ;
-    p.drawLine(400+set[1],-100+set[0],400+set[1],1000+set[0]);
-    p.drawLine(0+set[1],400+set[0],900+set[1],400+set[0]);
+//    float* set = twodObj->getMinXY() ;
+//    p.drawLine(400+set[1],-100+set[0],400+set[1],1000+set[0]);
+//    p.drawLine(0+set[1],400+set[0],900+set[1],400+set[0]);
 
 
 //    for (int i = 1; i < SIZE; ++i){
@@ -378,11 +402,26 @@ int main(int argc, char *argv[])
 //     }
 
     p.end(); // Don't forget this line!
-
+    p2.end();
+    p3.end();
+    p4.end();
     l.setPicture(pi);
+//    QString an1 = QString("Front View");
+//    l.setText(an1);
+//    QString an2 = QString("Side View") ;
+//    l2.setText(an2);
+//    QString an3 = QString("Top View") ;
+//    l3.setText(an3);
+//    QString an4 = QString("Isometric View");
+//    l4.setText(an4);
     l.show();
-    QLabel l2 ;
-    l2.setPicture(pi);
+
+//    QLabel l2 ;
+    l2.setPicture(pi2);
     l2.show();
+    l3.setPicture(pi3);
+    l3.show();
+    l4.setPicture(pi4);
+    l4.show();
     return a.exec();
 }
